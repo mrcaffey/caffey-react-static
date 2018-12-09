b3409317aaa8b356dd8d8208e238e89209030276
Micah Caffey <mrcaffey@gmail.com>
2018-12-08 22:33:41 -0700

    latest changes

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,5 +6,6 @@ export default () => (
   <div>
     <h1>Caffey, Inc. Contact Information</h1>
     <p>Ideally, we'd have contact process here to limit spam.</p>
+    <p>Need detailed contact form linking to admin email for quick response</p>
   </div>
 )
