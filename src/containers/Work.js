aaf703a0d8c4e141d40b6269db733f910ca723fb
Micah Caffey <mrcaffey@gmail.com>
2019-01-07 20:38:46 -0700

    added items to Caffey projects

--- src/containers/Work.js
+++ src/containers/Work.js
@@ -5,8 +5,22 @@ import React from 'react'
 export default () => (
   <div>
     <h1>Caffey, Inc. Projects</h1>
-    <p>Here, i'd like to have a React-based project map which takes in "active" Procore jobs and automatically 
-      adds them to a map under a maroon shade. Upon zooming into map, active sites are shown.
-    </p>
+    <h2>Formally Contracted</h2>
+    <ul>
+      <li>Project 1</li>
+    </ul>
+    <h2>In Production</h2>
+    <ul>
+      <li>Ochsner Medical Center - Northshore Campus Room Renovation, Slidell, LA.</li>
+      <li>Keesler Air Force Base Medical Center - AGFA Room Renovation, Biloxi, MS.</li>
+      <li>JACC Pensacola - Philips MRI Room Renovation, Pensacola, FL.</li>
+      <li>Hilo Medical Center MRI Room Renovation, Hilo, Hawai'i</li>
+      <li>Project 1</li>
+      <li>Project 1</li>
+      <li>Project 1</li>
+      <li>Project 1</li>
+    </ul>
+    <h2>In Design</h2>
+    <p>Baton Rouge North Clinic - Canon</p>
   </div>
 )
