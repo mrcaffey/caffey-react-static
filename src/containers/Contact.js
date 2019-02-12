51ca4dd21a20dad82ba732a317d8fb5af1d6e4a4
Micah Caffey <mrcaffey@gmail.com>
2019-02-11 17:34:39 -0700

    changes

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ContactForm extends Component{
     render(){
         return(
-            <div style= {{ paddingTop: '100px' }} className="col-lg-4 offset-lg-4">
+            <div style= {{ paddingTop: '100px' }}>
             <p>Please fill out the prefilled form on your preferred email account. Thank you!</p>
                     <a href="mailto:jp.buckley@caffeyinc.com?subject=New%20Caffey%20Inc.%20Inquiry&body=Name:%0D%0APhone:%0D%0A%0D%0AMessage:%0D%0A">Send us an Email</a>
            </div>
