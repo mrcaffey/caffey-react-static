8942e013ce4c138eea4222d0fd314d4a43c48f91
Micah <mrcaffey@gmail.com>
2019-04-24 17:05:55 -0600

    changes to description

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -4,7 +4,7 @@ class ContactForm extends Component{
     render(){
         return(
             <div style= {{ paddingTop: '100px', paddingBottom: '100px'}}>
-            <p>Please fill out the prefilled form on your preferred email account. Thank you!</p>
+            <p>Please click to fill out this form on your email account. Thank you!</p>
                 <a href="mailto:jp.buckley@caffeyinc.com?subject=New%20Caffey%20Inc.%20Inquiry&body=Name:%0D%0APhone:%0D%0A%0D%0AMessage:%0D%0A">Send us an Email</a>
                 <br/>
                 <br/>
